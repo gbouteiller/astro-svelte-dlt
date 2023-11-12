bbad5aa97312a8501cd89e9ad46ebab76d2f39c0
Gregory Bouteiller <gregory.bouteiller@niama.re>
2023-11-12 21:47:58 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ export default defineConfig({
   output: 'server',
   integrations: [tailwind(), svelte()],
   adapter: vercel({
-    imageService: true
+    imagesConfig: {domains: [], formats: ['image/avif', 'image/webp'], sizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840]},
+    imageService: true,
   }),
   image: {
     domains: ['utfs.io'],
