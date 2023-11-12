f4f022fe88faee4077afb751c789878f91df4ad4
Gregory Bouteiller <gregory.bouteiller@niama.re>
2023-11-12 21:43:38 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import {defineConfig} from 'astro/config';
-
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), svelte()],
-  adapter: vercel(),
+  adapter: vercel({
+    imageService: true
+  }),
   image: {
-    domains: ["utfs.io"],
-  }
+    domains: ['utfs.io'],
+  },
 });
